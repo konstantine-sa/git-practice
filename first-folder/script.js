58138832dd75818cd597b50ede761c4c4316d47f
Konstantin Savchenko <konstantine.sa@gmail.com>
2023-02-26 12:59:38 +0100

    added light on/off feature

--- first-folder/script.js
+++ first-folder/script.js
@@ -5,5 +5,16 @@ function off() {
   document.querySelector(".out").innerHTML = ``;
 }
 
+function lightOff() {
+  document.querySelector("body").style = `
+        background-color: black;
+      `;
+}
+function lightOn() {
+  document.querySelector("body").style = ``;
+}
+
 document.querySelector(".on").onclick = on;
 document.querySelector(".off").onclick = off;
+document.querySelector(".light-off").onclick = lightOff;
+document.querySelector(".light-on").onclick = lightOn;
